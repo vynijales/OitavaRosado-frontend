1c68ebcb0b86e5149d506228727e9a9754ccb8f1
vynijales <vynijales@gmail.com>
2024-07-25 10:02:52 -0300

    refactor: organizar estilização

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,15 +13,15 @@ export default function AuthLayout({
     children: React.ReactNode
 }) {
     return (
-        <main className="flex h-screen w-full items-center justify-center">
+        <main className="w-full h-screen flex items-center justify-center">
 
             <div
                 id="container"
-                className="h-full xl:h-[80vh] w-full xl:w-[75vw] min-w-[500px] flex flex-col xl:flex-row overflow-hidden rounded-md shadow-lg"
+                className="w-full xl:w-[75vw] min-w-[500px] h-full xl:h-[80vh] flex flex-col xl:flex-row overflow-hidden rounded-md shadow-lg"
             >
                 <section
                     id="login-aside-logo"
-                    className={`h-1/5 xl:h-full xl:w-3/5 flex flex-row-reverse xl:flex-col items-center justify-center gap-4 bg-primary text-white select-none p-4`}
+                    className={`xl:w-3/5 h-1/5 xl:h-full flex flex-row-reverse xl:flex-col items-center justify-center gap-4 bg-primary text-white select-none p-4`}
                 >
                     <Image
                         src={Logo}
@@ -32,7 +32,7 @@ export default function AuthLayout({
 
                 <section
                     id="form-login"
-                    className="flex h-full w-auto flex-grow flex-col items-center justify-center gap-1 bg-white p-20"
+                    className="w-auto xl:w-2/5 h-full flex flex-grow flex-col items-center justify-center gap-1 bg-white p-20"
                 >
                     {children}
 
