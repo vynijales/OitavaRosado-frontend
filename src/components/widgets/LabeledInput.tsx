af33c74efb48bb62a547d10e64dba8f0eff8ed83
vynijales <vynijales@gmail.com>
2024-07-28 15:26:09 -0300

    feat: criar componentes de input e select personalizados

---
+++ src/components/widgets/LabeledInput.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface LabeledInputProps {
+    label?: string;
+    name: string;
+    type: string;
+    placeholder?: string;
+    className?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function LabeledInput({ label, name, type, placeholder, className, value, onChange }: LabeledInputProps) {
+    const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+    return (
+        <div className={`relative ${className}`}>
+            <Label htmlFor={name} className="absolute translate-x-2 -translate-y-2 z-[2] text-xs bg-white">
+                { label ? label : nameCapitalized }
+            </Label>
+            <Input
+                type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder ? placeholder : nameCapitalized}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
---
+++ src/components/widgets/LabeledSelect.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+interface Options {
+    value: string;
+    label: string;
+}
+
+interface LabeledSelectProps {
+    name: string;
+    options: Options[];
+    label?: string;
+    value?: string;
+    placeholder?: string;
+    className?: string;
+    onChange?: (value: string) => void;
+}
+
+export function LabeledSelect({ label, name, options, value, placeholder, className, onChange }: LabeledSelectProps) {
+    const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+    const [selectedValue, setSelectedValue] = useState(value || options[0]?.value);
+
+    useEffect(() => {
+        if (value) {
+            setSelectedValue(value);
+        }
+    }, [value]);
+
+    const handleChange = (newValue: string) => {
+        setSelectedValue(newValue);
+        if (onChange) {
+            onChange(newValue);
+        }
+    };
+
+    return (
+        <div className={`relative ${className}`}>
+            <Label htmlFor={name} className="absolute translate-x-2 -translate-y-2 z-[2] text-xs bg-white">
+                {label ? label : nameCapitalized}
+            </Label>
+            <Select value={selectedValue} onValueChange={handleChange}>
+                <SelectTrigger className="w-full">
+                    <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                    {options.map((option, index) => (
+                        <SelectItem key={index} value={option.value}>{option.label}</SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        </div>
+    );
+}
