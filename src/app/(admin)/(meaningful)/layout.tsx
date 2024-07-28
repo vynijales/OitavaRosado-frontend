13fea8d14949aa47fde4ec88ced2b7addec77dfc
vynijales <vynijales@gmail.com>
2024-07-28 13:31:48 -0300

    style: pequena mudança no background

--- src/app/(admin)/(meaningful)/layout.tsx
+++ src/app/(admin)/(meaningful)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
 
     return (
         <>
-            <div className="absolute top-0 left-0 w-full h-2/5 bg-primary"></div>
+            <div className="absolute top-0 left-0 w-full h-[37vh] bg-primary"></div>
             <div className="w-full xl:w-4/5 z-0 flex flex-col self-center pt-6">
                 {children}
             </div>
