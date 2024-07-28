113151d17a7c85f6f2194eb3f7b7e9940f4eddee
vynijales <vynijales@gmail.com>
2024-07-28 17:01:50 -0300

    feat: criar componente de textarea personalizado

---
+++ src/components/widgets/LabeledTextArea.tsx
@@ -0,0 +1,30 @@
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+
+interface LabeledTextAreaProps {
+    label?: string;
+    name: string;
+    placeholder?: string;
+    className?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+
+export function LabeledTextArea({ label, name, placeholder, className, value, onChange }: LabeledTextAreaProps) {
+    const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+    return (
+        <div className={`relative ${className}`}>
+            <Label htmlFor={name} className="absolute translate-x-2 -translate-y-2 z-[2] text-xs bg-white">
+                { label ? label : nameCapitalized }
+            </Label>
+            <Textarea
+                id={name}
+                name={name}
+                placeholder={placeholder ? placeholder : nameCapitalized}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
