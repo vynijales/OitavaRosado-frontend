acd2767471f9af978932c353e8b07770ed6de7f0
vynijales <vynijales@gmail.com>
2024-07-28 13:54:11 -0300

    feat: acrescentar texto ao rodapé

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="w-full h-12 flex justify-end items-center gap-4 bg-gray-100 text-accent px-14">
-            <Link href="#">Feedback</Link>
-            <Link href="#">Centro de suporte</Link>
-            <Link href="#">Contato</Link>
+        <footer className="w-full h-12 flex justify-between items-center bg-gray-100 text-accent pl-28 pr-20">
+                © 2024 Clínica Oitava Rosado
+            <div className="flex justify-end items-center gap-4">
+                <Link href="#">Feedback</Link>
+                <Link href="#">Centro de suporte</Link>
+                <Link href="#">Contato</Link>
+            </div>
         </footer>
     );
 }
